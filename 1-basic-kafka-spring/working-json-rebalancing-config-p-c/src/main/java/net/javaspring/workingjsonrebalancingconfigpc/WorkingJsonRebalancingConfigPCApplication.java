782dbe8e54b1c7f9a29e5d3e2bb158188f5a5cbd
meher12 <meher.khemiri@gmail.com>
2022-09-09 11:02:35 +0100

    create second project

---
+++ 1-basic-kafka-spring/working-json-rebalancing-config-p-c/src/main/java/net/javaspring/workingjsonrebalancingconfigpc/WorkingJsonRebalancingConfigPCApplication.java
@@ -0,0 +1,13 @@
+package net.javaspring.workingjsonrebalancingconfigpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingJsonRebalancingConfigPCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingJsonRebalancingConfigPCApplication.class, args);
+	}
+
+}
---
+++ 1-basic-kafka-spring/working-json-rebalancing-config-p-c/src/test/java/net/javaspring/workingjsonrebalancingconfigpc/WorkingJsonRebalancingConfigPCApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaspring.workingjsonrebalancingconfigpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingJsonRebalancingConfigPCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
