4fcdf6d04e51a3afca73793667b77f1b12aa43ed
meher12 <meher.khemiri@gmail.com>
2022-09-11 15:22:17 +0100

    add producer and consumer kafka config

---
+++ 1-basic-kafka-spring/working-json-rebalancing-config-producer-consumer/kafka-consumer/src/main/java/net/javaspring/kafkaconsumer/config/KafkaConsumerConfig.java
@@ -0,0 +1,28 @@
+package net.javaspring.kafkaconsumer.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Autowired
+    private KafkaProperties kafkaProperties;
+
+    @Bean
+    public ConsumerFactory<Object, Object> consumerFactory() {
+        var properties = kafkaProperties.buildConsumerProperties();
+
+        properties.put(ConsumerConfig.METADATA_MAX_AGE_CONFIG, "120000"); // 2 minutes
+
+        return new DefaultKafkaConsumerFactory<Object, Object>(properties);
+    }
+
+
+}
